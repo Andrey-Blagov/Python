73f3f3236d3691593a37a7886d80b387e66f88a2
Andrey <blagovandrey1323@gmail.com>
2023-10-28 14:42:06 +0300

    Add main prog anal

---
+++ Seminars/Sem1Analit/anal_sem1.py
@@ -0,0 +1,32 @@
+# import this
+# from math import ceil
+# n = int(input("Введите скорость автомобиля: "))
+# m = int(input("Введитие общее расстояние: "))
+#
+# print(ceil(m/n))
+
+# print(m//n + (m % n > 0))
+
+# print((m + n - 1) // n)
+
+
+
+# x = int(input("Qty of student in 1 class: "))
+# y = int(input("Qty of student in 2 class: "))
+# z = int(input("Qty of student in 3 class: "))
+#
+# print(f"It is required to buy {(-(-x//2) - (-y//2) - (-z//2))} scool desks")
+# print(f"It is required to buy {((x+1)//2) + ((y+1)//2) + ((z+1)//2)} scool desks")
+
+# a, b, c = 21, 21, 23
+#
+# print(a // 2 + a % 2 + b // 2 + b % 2 + c // 2 + c % 2)
+#
+# total_desks = 0
+# for i in a, b, c:
+#     total_desks += i // 2 + i % 2
+# print(f"Всего надо {total_desks} парт")
+
+
+
+
