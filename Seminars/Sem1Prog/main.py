15c39038091f463b42cfcaa42f300b16a2eeb5a4
Andrey <blagovandrey1323@gmail.com>
2023-10-28 11:15:50 +0300

    Add main prog

---
+++ Seminars/Sem1Prog/main.py
@@ -0,0 +1,31 @@
+# import this
+# import math
+# n = int(input("Введите скорость автомобиля: "))
+# m = int(input("Введитие общее расстояние: "))
+
+# print(math.ceil(m/n))
+
+# print(m//n + (m % n > 0))
+
+# print((m + n - 1) // n)
+
+
+
+# x = int(input("Qty of student in 1 class: "))
+# y = int(input("Qty of student in 2 class: "))
+# z = int(input("Qty of student in 3 class: "))
+
+# print(f"It is required to buy {(-(-x//2) - (-y//2) - (-z//2))} scool desks")
+# print(f"It is required to buy {((x+1)//2) + ((y+1)//2) + ((z+1)//2))} scool desks")
+
+
+
+# x = int(input("В какой сел: "))
+# y = int(input("Сколько прошёл: "))
+
+# if x == y:
+#     print("Невозможно подсчитать колличество вагонов!")
+# else:
+#     print("Всего ", x + y - 1, " вагонов")
+
+
